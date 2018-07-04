e430d82f7c97fd93f3b07e387885c42e3192d14c
Thomas MacLean <thomas.maclean@marlon.be>
2018-07-04 13:10:19 +0200

    eslint + vuex

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,14 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
   }
 }
