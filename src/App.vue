8283251ad661f808ef5f5f291220286234629800
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-07-12 22:42:43 +0200

    l

--- src/App.vue
+++ src/App.vue
@@ -21,111 +21,107 @@
 </template>
 
 <script>
-  export default {
-      name: 'app',
-      data() {
-          return {
-              email: null
-          };
-      },
-      methods: {
-          logout() {
-              localStorage.removeItem('googleToken');
-              localStorage.removeItem('token');
-              var auth2 = gapi.auth2.getAuthInstance();
-              if (auth2) {
-                  auth2.signOut().then(function () {
-                      console.log('User signed out.');
-                  });
-              }
-              this.email = null;
-              this.$router.push('/login');
-          },
-          setEmail(email) {
-              this.email = email;
-          }
-      }
-
-  };
-
+export default {
+    name: 'app',
+    data() {
+        return {
+            email: null
+        };
+    },
+    methods: {
+        logout() {
+            localStorage.removeItem('googleToken');
+            localStorage.removeItem('token');
+            var auth2 = gapi.auth2.getAuthInstance();
+            if (auth2) {
+                auth2.signOut().then(function() {
+                    console.log('User signed out.');
+                });
+            }
+            this.email = null;
+            this.$router.push('/login');
+        },
+        setEmail(email) {
+            this.email = email;
+        }
+    }
+};
 </script>
 
 <style>
-  body {
+body {
+  height: 100vh;
+  margin: 0;
+  background: var(--color-three);
+}
 
-    height: 100vh;
-    margin: 0;
-    background: var(--color-three);
+.pull-right {
+  float: right;
+}
 
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: var(--color-one);
 
-  .pull-right {
-    float: right;
-  }
+  height: 100vh;
+}
 
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: var(--color-one);
+main {
+  margin: 40px 0;
+  min-height: calc(100vh - 196px);
+  background: var(--color-three);
+}
 
-    height: 100vh;
-  }
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
 
+  background-color: var(--color-one);
+  color: #ffffff;
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+}
 
-  main {
-    margin: 40px 0;
-    min-height: calc(100vh - 196px);
-    background: var(--color-three);
-  }
+.logout-btn {
+  color: white;
+  border: 1px solid white;
+}
 
-  header {
-    margin: 0;
-    height: 56px;
-    padding: 0 16px 0 24px;
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
 
-    background-color: var(--color-one);
-    color: #ffffff;
-    display: flex;
-    align-items: baseline;
-    justify-content: space-between;
-  }
+  letter-spacing: 0.02em;
 
-  .logout-btn {
-    color: white;
-    border: 1px solid white;
-  }
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
 
-  header span {
-    display: block;
-    position: relative;
-    font-size: 20px;
-    line-height: 1;
-
-    letter-spacing: .02em;
-
-    font-weight: 400;
-    box-sizing: border-box;
-    padding-top: 16px;
-  }
-
-
-  footer {
-    height: 60px;
-    background: var(--color-four);
-    width: 100vw;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: medium;
-  }
-
-  .emoji {
-    text-decoration: none;
-    cursor:help;
-    font-size: large;
-    font-style: normal;
-  }
-
+footer {
+  height: 60px;
+  background: var(--color-four);
+  width: 100vw;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: medium;
+}
+footer code {
+  background: transparent;
+  border: none;
+}
+.emoji {
+  text-decoration: none;
+  cursor: help;
+  font-size: large;
+  font-style: normal;
+}
 </style>
