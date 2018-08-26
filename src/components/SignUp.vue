90e50a9cf618edcb8c0a12f0d04e8cc096aa4543
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-08-27 00:34:24 +0200

    nav to login

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -11,82 +11,84 @@
                     <input type="submit" :disabled="email===''||password===''" value="signup" class="button-primary" />
                 </form>
             </div>
+            <div @click="login">login</div>
         </div>
     </div>
 </template>
 
 <script>
-    import {
-        api
-    } from '../api/api';
+import { api } from '../api/api';
 
-    export default {
-        name: 'Login',
-        mounted() {
-            //fetch(api + 'users').then(x => x.json()).then(y => console.log(y));
-        },
-        data() {
-            return {
-                msg: 'Sign up',
-                email: null,
-                password: null
-            };
-        },
-        methods: {
-            signup() {
-                console.log(this.email);
-                console.warn(this.password);
-                fetch(api + 'signup', {
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    method: 'POST',
-                    body: JSON.stringify({
-                        email: this.email,
-                        password: this.password
-                    })
-                }).then(x => x.json().then(y => {
+export default {
+    name: 'SignUp',
+    mounted() {
+        //fetch(api + 'users').then(x => x.json()).then(y => console.log(y));
+    },
+    data() {
+        return {
+            msg: 'Sign up',
+            email: null,
+            password: null
+        };
+    },
+    methods: {
+        signup() {
+            console.log(this.email);
+            console.warn(this.password);
+            fetch(api + 'signup', {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    email: this.email,
+                    password: this.password
+                })
+            }).then(x =>
+                x.json().then(y => {
                     console.log(y);
                     localStorage.setItem('token', y.token);
                     localStorage.setItem('to2do', true);
 
                     this.$router.push('/Confirm');
-                }));
-            }
+                })
+            );
+        },
+        login() {
+            this.$router.push('/Login');
         }
-    };
-
+    }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-    h1,
-    h2 {
-        font-weight: normal;
-    }
+h1,
+h2 {
+    font-weight: normal;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+    list-style-type: none;
+    padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
 
-    a {
-        color: var(--color-one);
-    }
+a {
+    color: var(--color-one);
+}
 
-    .hello {
-        display: flex;
-        justify-content: center;
-    }
+.hello {
+    display: flex;
+    justify-content: center;
+}
 
-    .form-wrap {
-        max-width: 400px;
-    }
-
+.form-wrap {
+    max-width: 400px;
+}
 </style>
